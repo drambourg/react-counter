5f734ea26dd3a72bcae8c3330798bfddd242e7dc
xNousex <damien.rambourg@gmail.com>
2019-08-14 11:12:13 +0200

    Mise en forme

--- src/Component/Counter.js
+++ src/Component/Counter.js
@@ -1,23 +1,33 @@
 import React, { useState } from 'react';
+import { Button, Input } from 'reactstrap';
+import Row from "reactstrap/es/Row";
 
 function Counter() {
     const [count, setCount] = useState(0);
 
     return (
-        <div>
-            <p>Le compteur est  à : {count} </p>
+        <div className="container">
 
-            <button onClick={() => setCount(count - 1)}>
+            <h3>Le compteur est  à : {count} </h3>
+            <Row className="justify-content-center ">
+            <Button className="col-2 m-1"
+                color="danger"
+                onClick={() => setCount(count - 1)}>
                 -1
-            </button>
-            <input
+            </Button>
+            <Input
+                className="col-5 text-center m-1"
                 name="count"
                 value={count}
                 onChange={(e) => setCount(parseInt(e.target.value))}
             />
-            <button onClick={() => setCount(count + 1)}>
+            <Button
+                className="col-2 m-1"
+                color="success"
+                onClick={() => setCount(count + 1)}>
                 +1
-            </button>
+            </Button>
+            </Row>
         </div>
     );
 }
